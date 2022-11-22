27320887cfc056a3ffab34880a5b6f1b6780018e
Nikita Lipin <nikitalipin97@gmail.com>
2022-11-22 20:37:59 +0300

    wip

--- src/lib/apiError.ts
+++ src/lib/apiError.ts
@@ -1,6 +1,7 @@
 import { NextApiResponse } from "next";
 
 export const apiErrorHandle = (error: any, res: NextApiResponse) => {
+  console.log(error)
   const data = error?.response?.data
 
   data
