c11082511837b089af35b0b4990e53690be21def
Nikita Lipin <nikitalipin97@gmail.com>
2022-12-31 12:43:01 +0300

    test

--- src/lib/redirectHandler.ts
+++ src/lib/redirectHandler.ts
@@ -8,37 +8,38 @@ export function redirectHandler(url: string) {
     req: NextApiRequest,
     res: NextApiResponse
   ) {
-    try {
-      const { body, query, headers, method } = req;
-      const axiosMethod = method!.toLowerCase() as Methods
+    res.send({})
+    // try {
+    //   const { body, query, headers, method } = req;
+    //   const axiosMethod = method!.toLowerCase() as Methods
       
-      const queryString = Object.entries(query)
-      .reduce((result, [key, value]) => {
-        const divider = result === '' ? '?' : '&'
-        return `${result}${divider}${key}=${value}`
-      }, '')
-      console.log(method, `${process.env.API}${url}${queryString}`)
+    //   const queryString = Object.entries(query)
+    //   .reduce((result, [key, value]) => {
+    //     const divider = result === '' ? '?' : '&'
+    //     return `${result}${divider}${key}=${value}`
+    //   }, '')
+    //   console.log(method, `${process.env.API}${url}${queryString}`)
 
-      const { data, headers: returnedHeaders, status } = await axios({
-        url: `${process.env.API}${url}${queryString}`,
-        method: axiosMethod,
-        headers,
-        data: body,
-      })
+    //   const { data, headers: returnedHeaders, status } = await axios({
+    //     url: `${process.env.API}${url}${queryString}`,
+    //     method: axiosMethod,
+    //     headers,
+    //     data: body,
+    //   })
 
-      //? Подставляю оригинальные хэдеры с куками в прокси ответ
-      Object.entries(returnedHeaders).forEach(([headerKey, value]) => {
-        res.setHeader(headerKey, value as string)
-      })
+    //   //? Подставляю оригинальные хэдеры с куками в прокси ответ
+    //   Object.entries(returnedHeaders).forEach(([headerKey, value]) => {
+    //     res.setHeader(headerKey, value as string)
+    //   })
   
-      res.status(status).json(data)
-    } catch (error: any) {
-      const axiosError = error as AxiosError
+    //   res.status(status).json(data)
+    // } catch (error: any) {
+    //   const axiosError = error as AxiosError
   
-      res
-        .status(axiosError.response?.status || 500)
-        .json(axiosError.response?.data || axiosError.message)
-    }
+    //   res
+    //     .status(axiosError.response?.status || 500)
+    //     .json(axiosError.response?.data || axiosError.message)
+    // }
   }
 }
 
