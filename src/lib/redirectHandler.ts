1a109315b90f362d0885166a82b00eaa01edca4e
Nikita Lipin <nikitalipin97@gmail.com>
2022-12-31 12:35:41 +0300

    wip

--- src/lib/redirectHandler.ts
+++ src/lib/redirectHandler.ts
@@ -11,12 +11,13 @@ export function redirectHandler(url: string) {
     try {
       const { body, query, headers, method } = req;
       const axiosMethod = method!.toLowerCase() as Methods
-
+      
       const queryString = Object.entries(query)
-        .reduce((result, [key, value]) => {
-          const divider = result === '' ? '?' : '&'
-          return `${result}${divider}${key}=${value}`
-        }, '')
+      .reduce((result, [key, value]) => {
+        const divider = result === '' ? '?' : '&'
+        return `${result}${divider}${key}=${value}`
+      }, '')
+      console.log(method, `${process.env.API}${url}${queryString}`)
 
       const { data, headers: returnedHeaders, status } = await axios({
         url: `${process.env.API}${url}${queryString}`,
