cb7090a4df186d02faf1d8a2de7646255ea9d7a5
Nikita Lipin <nikitalipin97@gmail.com>
2023-01-10 22:16:58 +0300

    remove test button

--- src/components/Chats/Chats.tsx
+++ src/components/Chats/Chats.tsx
@@ -1,22 +1,18 @@
 import { memo } from "react"
-import { Button, Center, Flex, Spinner, Text } from "@chakra-ui/react"
+import { Center, Flex, Spinner, Text } from "@chakra-ui/react"
 import { useAppSelector } from "@/hooks/useAppSelector"
 import { useAppDispatch } from "@/hooks/useAppDispatch"
 
 import { ChatItem } from "../ChatItem/ChatItem"
 import { chatActions } from "@/redux/slices/chat.slice"
 import { useChatsQuery } from "@/redux/api/chat"
-import { IChat, IUser } from "@/types"
+import { IChat } from "@/types"
 
 
 export const Chats = memo(() => {
   const dispatch = useAppDispatch()
   const { isLoading } = useChatsQuery('')
   const { activeChat, chats } = useAppSelector(state => state.chat)
-
-  const onClick = () => {
-    dispatch(chatActions.submitMessage({}))
-  }
 
   if (isLoading) {
     return (
@@ -30,7 +26,6 @@ export const Chats = memo(() => {
     return (
       <Center h='100%'>
         <Text>You don't have any active chats yet</Text>
-        <Button onClick={onClick}>Emit</Button>
       </Center>
     )
   }
